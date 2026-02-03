51bd6b37a97d6376cccd1b0fcf8c8817cf212043
Laryssa Siqueira <larysiqueira13@gmail.com>
2026-02-03 13:30:39 -0300

    Update SuperTrunfoNovato.c

--- SuperTrunfoNovato.c
+++ SuperTrunfoNovato.c
@@ -4,7 +4,6 @@
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
 
 int main()
 {
